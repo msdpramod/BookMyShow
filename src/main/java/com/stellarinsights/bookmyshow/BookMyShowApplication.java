2140bd6ff7095ff0d655ef69baff9c5205c51bf2
msdpramod <kuppapramod@gmail.com>
2023-12-29 09:01:34 +0530

    updated

--- src/main/java/com/stellarinsights/bookmyshow/BookMyShowApplication.java
+++ src/main/java/com/stellarinsights/bookmyshow/BookMyShowApplication.java
@@ -10,6 +10,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+// @EnableJpaAuditing is used to enable the auditing feature
+// Auditing is a feature of Spring Data JPA that automatically populates auditing-related fields
+// @CreatedDate, @CreatedBy, @LastModifiedDate, and @LastModifiedBy
+// in your entities when they are created and updated.
+// This feature is very useful because it eliminates the need to manually set these fields
+// and also provides a way to store additional information such as who created or updated an entity,
+// and when the entity was created or updated.
+// To enable auditing, you need to add the @EnableJpaAuditing annotation to your main class.
+// This annotation takes one parameter, which is the name of the auditor class.
+// The auditor class is a class that implements the AuditorAware interface.
+// This interface has one method, getCurrentAuditor(), which returns an Optional object.
+// The Optional object contains the current auditor of the application.
+//
 public class BookMyShowApplication implements CommandLineRunner {
     private UserController userController;
 
--- src/main/java/com/stellarinsights/bookmyshow/Models/BaseModel.java
+++ src/main/java/com/stellarinsights/bookmyshow/Models/BaseModel.java
@@ -2,23 +2,35 @@ package com.stellarinsights.bookmyshow.Models;
 
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @MappedSuperclass
 // MappedSuperclass is used when you want to inherit properties
 // from a class but you don't want to map that class to the database.
 @Getter
 @Setter
+@EntityListeners({AuditingEntityListener.class})
+// AuditingEntityListener is used to automatically populate the createdAt and updatedAt fields
 public class BaseModel {
     @Id @GeneratedValue( strategy = GenerationType.AUTO)
     // @GeneratedValue annotation is used to specify the primary key generation strategy to use
     // for the primary key. The strategies can be either to identity, sequence or table.
     // GenerationType.AUTO is the default generation type and lets the persistence provider
     private Long id;
+    @CreatedDate
+    @Temporal(TemporalType.TIME)
+    private Date createdDate;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedAt;
 
 }
